b08b8de018ceb363fe98e63bb5b1ae91b6b87e02
noprom <tyee.noprom@qq.com>
2015-07-10 21:54:50 +0800

    Initial Commit

---
+++ SimpleTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleTableView
+//
+//  Created by noprom on 15/7/10.
+//  Copyright (c) 2015年 noprom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimpleTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleTableView
+//
+//  Created by noprom on 15/7/10.
+//  Copyright (c) 2015年 noprom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
